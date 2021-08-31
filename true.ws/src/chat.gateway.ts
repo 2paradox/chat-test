829de748fa3018160ab61fe357937a013d248086
2paradox <dltjdcjf93@likelion.org>
2021-09-01 07:32:22 +0900

    platform-socket.io 버전만 다름 나머진 최신

---
+++ true.ws/src/chat.gateway.ts
@@ -0,0 +1,53 @@
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway(81, { namespace: 'chat' })
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: Server){
+    this.logger.log("Initialized!");
+  }
+  
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('welcome')
+  connectSomeone(
+    @MessageBody() data: string,
+  ) {
+    const [nickname, room] = data;
+    console.log(`${nickname}님이 코드: ${room}방에 접속했습니다.`);
+    const comeOn = `${nickname}님이 입장했습니다.`;
+    this.server.emit('comeOn' + room, comeOn);
+  }
+
+  private broadcast(event, client, message: any) {
+    for (let id in this.server.sockets)
+      if (id !== client.id) this.server.sockets[id].emit(event, message);
+  }
+
+  @SubscribeMessage('send')
+  sendMessage(@MessageBody() data: string, @ConnectedSocket() client) {
+    const [room, nickname, message] = data;
+    console.log(`${client.id} : ${data}`);
+    this.broadcast(room, client, [nickname, message]);
+  }
+}
